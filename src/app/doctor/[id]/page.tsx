37bd85905abf96bf68f05396ea6d4610c6c85db4
AlbinJo5 <albinjo12345alex@gmail.com>
2024-07-12 09:55:25 +0530

    color

---
+++ src/app/doctor/[id]/page.tsx
@@ -0,0 +1,20 @@
+import ColourCard from "@/components/Development/colourCard";
+import { Spacer } from "@nextui-org/react";
+import React from "react";
+
+function DoctorDetails() {
+  return (
+    <section>
+      <ColourCard colour="bg-red-200" height="h-[300px]" />
+      <Spacer y={2} />
+      <ColourCard height="h-[200px]" />
+      <Spacer y={2} />
+      <div className="grid grid-cols-2 gap-5">
+        <ColourCard colour="bg-green-200" height="h-[200px]" />
+        <ColourCard colour="bg-green-200" height="h-[200px]" />
+      </div>
+    </section>
+  );
+}
+
+export default DoctorDetails;
